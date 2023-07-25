4b7535a77fcf1bc4608b13ad3d73662bd3dcf7e0
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2023-07-25 05:08:42 +0500

    fix: statusline color fg

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -4,29 +4,11 @@ return {
     config = function(plugin, opts)
       local get_hlgroup = require("astronvim.utils").get_hlgroup
       -- use helper function to get highlight group properties
-      local comment_fg = get_hlgroup("Comment").fg
-      local git_branch_fg = comment_fg
-      local git_added = comment_fg
-      local git_changed = comment_fg
-      local git_removed = comment_fg
-      local blank_bg = get_hlgroup("FoldColumn").fg
+      local blank_bg = get_hlgroup("Comment").fg
       local git_bg = get_hlgroup("ColorColumn").bg
-      local git_fg = get_hlgroup("Delimiter").fg
-      local nav_icon_bg = get_hlgroup("String").fg
-      local nav_fg = plugin.nav_icon_bg
-      local folder_icon_bg = get_hlgroup("Error").fg
-
+      -- local git_fg = get_hlgroup("Delimiter").fg
+      local git_fg = get_hlgroup("FloatTitle").fg
       local status = require "astronvim.utils.status"
-      -- opts.statusline[1] = status.component.mode { mode_text = { padding = { left = 1, right = 1 } } } -- add the mode
-      -- opts.statusline[1] = status.component.mode {
-      --   mode_text = { icon = { kind = "VimIcon", padding = { right = 1, left = 1 } } },
-      --   surround = {
-      --     -- it's a left element, so use the left separator
-      --     separator = "left",
-      --     -- set the color of the surrounding based on the current mode using astronvim.utils.status module
-      --     -- color = function() return { main = status.hl.mode_bg(), right = blank_bg } end,
-      --   },
-      -- }
       opts.statusline = {
         hl = { fg = "fg", bg = "bg" },
         -- status.component.mode(),
