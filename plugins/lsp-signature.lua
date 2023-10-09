0f99c468646e4f28a432684bced080daa4783e6c
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2023-10-09 10:56:26 +0500

    fix: lsp

--- plugins/lsp-signature.lua
+++ plugins/lsp-signature.lua
@@ -4,7 +4,23 @@ return {
   event = "BufRead",
   config = function()
     require("lsp_signature").setup {
+      doc_lines = 2,
+      max_height = 12,
+      max_width = 150,
       hint_enable = false,
+      floating_window = true,
+      hint_prefix = "⚙️ ",
+      hint_inline = function() return false end,
+      always_trigger = false,
+      transparency = 25,
+      handler_opts = {
+        -- border = "single",
+        -- border = "shadow",
+        border = "rounded",
+        -- border = "none",
+      },
+      toggle_key = "<M-e>",
+      timer_interval = 2000,
     }
   end,
 }
--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -5,8 +5,8 @@ return {
       follow_current_file = true,
       filtered_items = {
         visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
+        hide_dotfiles = true,
+        hide_gitignored = true,
       },
     },
   },
