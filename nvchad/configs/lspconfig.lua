3c96742fbc764d2765a66017ee629f9c902990f4
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2023-12-24 06:52:20 +0500

    fix(lua_ls): undefined global vim

--- nvchad/configs/lspconfig.lua
+++ nvchad/configs/lspconfig.lua
@@ -1,28 +1,43 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
-  "yamlls",
-  "lua_ls",
-  "dockerls",
-  "docker_compose_language_service",
-  "pyright",
-  "ruff_lsp",
-  "html",
-  "cssls",
-  "tsserver",
-  "clangd",
+	"yamlls",
+	"lua_ls",
+	"dockerls",
+	"docker_compose_language_service",
+	"pyright",
+	"ruff_lsp",
+	"html",
+	"cssls",
+	"tsserver",
+	"clangd",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
---
--- lspconfig.pyright.setup { blabla }
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				-- @see https://github.com/LuaLS/LuaJIT
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
+})
