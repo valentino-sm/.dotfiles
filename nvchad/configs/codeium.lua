897abf5c4aacf3c34c7ecd88efc9db0475cdd224
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2023-12-26 09:10:45 +0500

    refactor: split plugins to files

---
+++ nvchad/configs/codeium.lua
@@ -0,0 +1,25 @@
+return {
+  "Exafunction/codeium.vim",
+  cmd = "Codeium",
+  event = "InsertEnter",
+  -- init = function()
+  --   vim.g.codeium_enabled = 1
+  --   -- vim.g.codeium_disable_bindings = 1
+  --   -- vim.g.codeium_idle_delay = 1500
+  --   -- match function
+  -- end,
+  config = function()
+    vim.keymap.set("i", "<C-s>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+    vim.keymap.set("i", "<M-]>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+    vim.keymap.set("i", "<M-[>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+    vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+    -- vim.api.nvim_set_hl(0, "CodeiumSuggestion", { link = "Comment" })
+    -- vim.keymap.set("n", "<leader>;", function()
+    --   if vim.g.codeium_enabled == true then
+    --     vim.cmd "CodeiumDisable"
+    --   else
+    --     vim.cmd "CodeiumEnable"
+    --   end
+    -- end, { noremap = true, desc = "Toggle Codeium active" })
+  end,
+}
--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -26,72 +26,8 @@ local plugins = {
     opts = overrides.mason,
   },
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    -- opts = overrides.treesitter,
-    dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
-    },
-    opts = function(_, opts)
-      opts.textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["ak"] = { query = "@block.outer", desc = "around block" },
-            ["ik"] = { query = "@block.inner", desc = "inside block" },
-            ["ac"] = { query = "@class.outer", desc = "around class" },
-            ["ic"] = { query = "@class.inner", desc = "inside class" },
-            ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-            ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-            ["af"] = { query = "@function.outer", desc = "around function " },
-            ["if"] = { query = "@function.inner", desc = "inside function " },
-            ["al"] = { query = "@loop.outer", desc = "around loop" },
-            ["il"] = { query = "@loop.inner", desc = "inside loop" },
-            ["aa"] = { query = "@parameter.outer", desc = "around argument" },
-            ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]k"] = { query = "@block.outer", desc = "Next block start" },
-            ["]f"] = { query = "@function.outer", desc = "Next function start" },
-            ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
-          },
-          goto_next_end = {
-            ["]K"] = { query = "@block.outer", desc = "Next block end" },
-            ["]F"] = { query = "@function.outer", desc = "Next function end" },
-            ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
-          },
-          goto_previous_start = {
-            ["[k"] = { query = "@block.outer", desc = "Previous block start" },
-            ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-            ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
-          },
-          goto_previous_end = {
-            ["[K"] = { query = "@block.outer", desc = "Previous block end" },
-            ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-            ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            [">K"] = { query = "@block.outer", desc = "Swap next block" },
-            [">F"] = { query = "@function.outer", desc = "Swap next function" },
-            [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
-          },
-          swap_previous = {
-            ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
-            ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-            ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
-          },
-        },
-      }
-    end,
-  },
+  -- custom treesitter stuff
+  require "custom.configs.treesitter",
 
   {
     "nvim-tree/nvim-tree.lua",
@@ -130,31 +66,7 @@ local plugins = {
     end,
   },
 
-  {
-    "Exafunction/codeium.vim",
-    cmd = "Codeium",
-    event = "InsertEnter",
-    -- init = function()
-    --   vim.g.codeium_enabled = 1
-    --   -- vim.g.codeium_disable_bindings = 1
-    --   -- vim.g.codeium_idle_delay = 1500
-    --   -- match function
-    -- end,
-    config = function()
-      vim.keymap.set("i", "<C-s>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-      vim.keymap.set("i", "<M-]>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-      vim.keymap.set("i", "<M-[>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-      vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
-      -- vim.api.nvim_set_hl(0, "CodeiumSuggestion", { link = "Comment" })
-      -- vim.keymap.set("n", "<leader>;", function()
-      --   if vim.g.codeium_enabled == true then
-      --     vim.cmd "CodeiumDisable"
-      --   else
-      --     vim.cmd "CodeiumEnable"
-      --   end
-      -- end, { noremap = true, desc = "Toggle Codeium active" })
-    end,
-  },
+  require "custom.configs.codeium",
 
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   {
@@ -180,56 +92,8 @@ local plugins = {
     config = true,
   },
 
-  {
-    "hiphish/rainbow-delimiters.nvim",
-    event = "BufReadPost",
-    config = function()
-      dofile(vim.g.base46_cache .. "rainbowdelimiters")
-      local rainbow_delimiters = require "rainbow-delimiters"
-
-      vim.g.rainbow_delimiters = {
-        strategy = {
-          [""] = rainbow_delimiters.strategy["global"],
-          vim = rainbow_delimiters.strategy["local"],
-        },
-        query = {
-          [""] = "rainbow-delimiters",
-          lua = "rainbow-blocks",
-        },
-        highlight = {
-          "RainbowDelimiterRed",
-          "RainbowDelimiterYellow",
-          "RainbowDelimiterBlue",
-          "RainbowDelimiterOrange",
-          "RainbowDelimiterGreen",
-          "RainbowDelimiterViolet",
-          "RainbowDelimiterCyan",
-        },
-      }
-    end,
-  },
-
-  {
-    "valentino-sm/neoscroll.nvim",
-    branch = "time-scale",
-
-    keys = { "<C-d>", "<C-u>" },
-    opts = {
-      time_scale = 0.5,
-      mappings = {
-        "<C-u>",
-        "<C-d>",
-      },
-      hide_cursor = true, -- Hide cursor while scrolling
-      stop_eof = true, -- Stop at <EOF> when scrolling downwards
-      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-      easing_function = "quadratic", -- Default easing function
-      pre_hook = nil, -- Function to run before the scrolling animation starts
-      post_hook = nil, -- Function to run after the scrolling animation ends
-      performance_mode = false, -- Disable "Performance Mode" on all buffers.
-    },
-  },
+  require "custom.configs.rainbow-delimiters",
+  require "custom.configs.neoscroll",
 }
 
 return plugins
