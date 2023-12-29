2cfada22729581e56e38a99a3ff6a9a36bd82d6c
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2023-12-30 01:33:28 +0500

    fix(zsh): delete failed history

--- zsh/oh-my-zsh-custom/delete-history.zsh
+++ zsh/oh-my-zsh-custom/delete-history.zsh
@@ -10,8 +10,10 @@ save-failed-command() {
 	local last_exit_code=$?
 
 	if [ "$last_exit_code" -ne "0" ] && [ "$last_exit_code" -ne "130" ]; then
-    FAILED_COMMANDS+="$(history | tail -1l | xargs | cut -d " " -f2-)"
     # FAILED_COMMANDS="$1\n$FAILED_COMMANDS"
+    # FAILED_COMMANDS+="$(history | tail -1l | xargs | cut -d " " -f2-)"
+    FAILED_COMMANDS+="$(history | tail -1l | sed 's/^[ \t]*//' | awk -F '  ' '{$1=$2=""; print $0}' | sed 's/^[ \t]*//;s/[ \t]*$//')"
+    # FAILED_COMMANDS+="$(history | tail -1l | sed 's/^[ \t]*//' | awk -F '  ' '{$1=$2=""; print $0}' | xargs)"
 	fi
 }
 
