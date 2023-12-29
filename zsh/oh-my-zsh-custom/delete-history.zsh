bd43a3dee808e7ff8b7c468203d61694169a8936
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2023-12-29 21:52:11 +0500

    feat: zsh

---
+++ zsh/oh-my-zsh-custom/delete-history.zsh
@@ -0,0 +1,23 @@
+exit_handler() {
+  for i in $FAILED_COMMANDS
+    do
+        hist d $i
+    done
+  FAILED_COMMANDS=()
+}
+
+save-failed-command() {
+	local last_exit_code=$?
+
+	if [ "$last_exit_code" -ne "0" ] && [ "$last_exit_code" -ne "130" ]; then
+    FAILED_COMMANDS+="$(history | tail -1l | xargs | cut -d " " -f2-)"
+    # FAILED_COMMANDS="$1\n$FAILED_COMMANDS"
+	fi
+}
+
+FAILED_COMMANDS=()
+# autoload -Uz add-zsh-hook
+precmd_functions+=(save-failed-command)
+zshexit_functions+=(exit_handler)
+# add-zsh-hook precmd save-failed-command
+# add-zsh-hook zshexit exit_handler
