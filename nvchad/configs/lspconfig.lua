bd0add555e41122176da934e4fc5aac7d7bd00a7
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2024-01-08 17:07:52 +0500

    fix(lspconfig): lua

--- nvchad/configs/lspconfig.lua
+++ nvchad/configs/lspconfig.lua
@@ -5,8 +5,8 @@ local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
+  -- "lua_ls", # nvchad does this itself
   "yamlls",
-  "lua_ls",
   "dockerls",
   "docker_compose_language_service",
   "pyright",
@@ -23,21 +23,3 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
-lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        -- @see https://github.com/LuaLS/LuaJIT
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-  },
-}
