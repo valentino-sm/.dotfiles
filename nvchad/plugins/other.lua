3a426fedf92367fa8e399b47d13b5e7a22e4df65
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2024-01-09 01:53:45 +0500

    feat(plugins): nvim-lastplace

--- nvchad/plugins/other.lua
+++ nvchad/plugins/other.lua
@@ -99,4 +99,14 @@ return {
       exclude_filetypes = { "NvimTree", "nvdash" },
     },
   },
+
+  {
+    "ethanholz/nvim-lastplace",
+    event = "BufRead",
+    opts = {
+      lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+      lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+      lastplace_open_folds = false,
+    },
+  },
 }
