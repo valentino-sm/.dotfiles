fbb371bded68b05931801e90e621e304db1814f3
valentino-sm <5059499+valentino-sm@users.noreply.github.com>
2025-04-08 13:37:43 +0500

    fix: chadrc.lua

--- nvchad/lua/chadrc.lua
+++ nvchad/lua/chadrc.lua
@@ -1,44 +1,67 @@
 ---@type ChadrcConfig
+
+vim.cmd("highlight St_relativepath guifg=#626a83 guibg=#2a2b36")
+
+local stbufnr = function()
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+end
+
+
 local M = {}
 
 -- Path to overriding theme and highlights files
 local highlights = require "highlights"
 
 M.ui = {
-  theme = "catppuccin-mocha",
-  theme_toggle = { "catppuccin-mocha", "one_light" },
+  -- theme = "catppuccin-mocha",
+  -- theme_toggle = { "catppuccin-mocha", "one_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+  -- hl_override = highlights.override,
+  -- hl_add = highlights.add,
 
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      [[                                                          ]],
-      [[                                                          ]],
-      [[                                                          ]],
-      [[            ██  ████████  ██                              ]],
-      [[            ████████████████                              ]],
-      [[            ██████████████████                            ]],
-      [[          ████  ████  ██████████                          ]],
-      [[          ████████████████████████                        ]],
-      [[          ██████    ██████████████████                    ]],
-      [[          ██  ██  ████  ████████████████████████████      ]],
-      [[          ████        ████████████████████████            ]],
-      [[          ████████████████████████████████████            ]],
-      [[          ████████████████████████████████████            ]],
-      [[          ████████████████████████████████████            ]],
-      [[          ████████████████████████████████████            ]],
-      [[          ██████████████████████████████████              ]],
-      [[            ████████████████████████████████              ]],
-      [[            ████    ████        ████    ████              ]],
-      [[            ████    ████        ████    ████              ]],
-      [[            ██      ██          ██      ██                ]],
+  -- nvdash = {
+  --   load_on_startup = true,
+  --   header = {
+  --     [[                                                          ]],
+  --     [[                                                          ]],
+  --     [[                                                          ]],
+  --     [[            ██  ████████  ██                              ]],
+  --     [[            ████████████████                              ]],
+  --     [[            ██████████████████                            ]],
+  --     [[          ████  ████  ██████████                          ]],
+  --     [[          ████████████████████████                        ]],
+  --     [[          ██████    ██████████████████                    ]],
+  --     [[          ██  ██  ████  ████████████████████████████      ]],
+  --     [[          ████        ████████████████████████            ]],
+  --     [[          ████████████████████████████████████            ]],
+  --     [[          ████████████████████████████████████            ]],
+  --     [[          ████████████████████████████████████            ]],
+  --     [[          ████████████████████████████████████            ]],
+  --     [[          ██████████████████████████████████              ]],
+  --     [[            ████████████████████████████████              ]],
+  --     [[            ████    ████        ████    ████              ]],
+  --     [[            ████    ████        ████    ████              ]],
+  --     [[            ██      ██          ██      ██                ]],
+  --   },
+  --   -- buttons = {},
+  -- },
+
+  statusline = require "configs.statusline",
+  statusline = {
+    theme = "default",
+    order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    modules = {
+      relativepath = function()
+        local path = vim.api.nvim_buf_get_name(stbufnr())
+
+        if path == "" then
+          return ""
+        end
+
+        return "%#St_relativepath#  " .. vim.fn.expand("%:.:h") .. " /"
+      end,
     },
-    -- buttons = {},
   },
-
-  -- statusline = require "configs.statusline",
 }
 
 M.base46 = {
